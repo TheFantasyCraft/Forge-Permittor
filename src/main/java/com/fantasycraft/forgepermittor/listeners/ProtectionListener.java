e9fd6341abfef675d92b283365b25b05d9c44ee1
thomas15v <tvanmellaerts@gmail.com>
2014-08-20 21:23:40 +0200

    Wtf Was this doing here!

--- src/main/java/com/fantasycraft/forgepermittor/listeners/ProtectionListener.java
+++ src/main/java/com/fantasycraft/forgepermittor/listeners/ProtectionListener.java
@@ -1,13 +1,9 @@
 package com.fantasycraft.forgepermittor.listeners;
 
 import com.fantasycraft.forgepermittor.ForgePermittor;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by thomas on 8/16/2014.
@@ -16,8 +12,6 @@ public class ProtectionListener implements Listener {
 
     @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent event){
-        List<Material> Ores = Arrays.asList(Material.COAL_ORE, Material.DIAMOND_ORE, Material.REDSTONE_ORE, Material.GOLD_ORE, Material.EMERALD_ORE, Material.IRON_ORE, Material.LAPIS_ORE);
-
         try {
             if (event.hasItem()){
                 ForgePermittor.log("ItemType: " + ForgePermittor.getInstance().getItemValidator().CheckItem(event.getItem()).toString(), true);
